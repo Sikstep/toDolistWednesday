9f36a961639e7b07dcc9e5758c4eac3b5fc42a63
Sikstep <Sikstep1988@gmail.com>
2023-04-12 15:49:21 +0300

    start again

--- src/index.tsx
+++ src/index.tsx
@@ -6,9 +6,12 @@ import AppWithRedux from './AppWithRedux';
 import {Provider} from 'react-redux';
 import {store} from './state/store';
 
-const container  = document.getElementById('root') as HTMLElement
+const container = document.getElementById('root') as HTMLElement
 const root = createRoot(container);
-root.render(<Provider store={store}><AppWithRedux/></Provider>);
+root.render(
+    <Provider store={store}>
+        <AppWithRedux/>
+    </Provider>);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
