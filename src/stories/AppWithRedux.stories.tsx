835fc75219163ae67311da7ede0cf935d44dc562
Sikstep <Sikstep1988@gmail.com>
2023-04-19 19:59:30 +0300

    Wednesday lesson #4 storyBook + addItemForm stories + Tasks stories + EditableSpan.stories.ts + AppWithRedux.stories.tsx

--- src/stories/AppWithRedux.stories.tsx
+++ src/stories/AppWithRedux.stories.tsx
@@ -1,47 +1,22 @@
-import type { Meta, StoryObj } from '@storybook/react';
-
-import { Button } from './Button';
+import type {Meta, StoryObj} from '@storybook/react';
+import AppWithRedux from '../AppWithRedux';
+import {ReduxStoreProviderDecorator} from './ReduxStoreProviderDecorator';
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
-const meta: Meta<typeof Button> = {
-  title: 'Example/Button',
-  component: Button,
+const meta: Meta<typeof AppWithRedux> = {
+  title: 'TODOLISTS/AppWithRedux',
+  component: AppWithRedux,
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
   tags: ['autodocs'],
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
+  decorators: [ReduxStoreProviderDecorator]
 };
 
 export default meta;
-type Story = StoryObj<typeof Button>;
+type Story = StoryObj<typeof AppWithRedux>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-export const Primary: Story = {
+export const AppWithDeduxStory: Story = {
   // More on args: https://storybook.js.org/docs/react/writing-stories/args
-  args: {
-    primary: true,
-    label: 'Button',
-  },
-};
-
-export const Secondary: Story = {
-  args: {
-    label: 'Button',
-  },
-};
-
-export const Large: Story = {
-  args: {
-    size: 'large',
-    label: 'Button',
-  },
-};
-
-export const Small: Story = {
-  args: {
-    size: 'small',
-    label: 'Button',
-  },
+  render: () => <AppWithRedux/>
 };
