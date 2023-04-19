cfc10a42c5d36907699bc681bdd9bad2d638996e
Sikstep <Sikstep1988@gmail.com>
2023-04-19 19:34:50 +0300

    Wednesday lesson #4 storyBook + addItemForm stories + Tasks stories + EditableSpan.stories.ts

--- src/stories/EditableSpan.stories.ts
+++ src/stories/EditableSpan.stories.ts
@@ -1,35 +1,47 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import {action} from '@storybook/addon-actions';
 
 import { Button } from './Button';
-import {TaskWithRedux} from '../TaskWithRedux';
-import {TaskType} from '../TodolistWithRedux';
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
-const meta: Meta<typeof TaskWithRedux> = {
-  title: 'TODOLISTS/TaskWithRedux',
-  component: TaskWithRedux,
+const meta: Meta<typeof Button> = {
+  title: 'Example/Button',
+  component: Button,
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
   tags: ['autodocs'],
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
-    // backgroundColor: { control: 'color' },
+    backgroundColor: { control: 'color' },
   },
 };
 
 export default meta;
-type Story = StoryObj<typeof TaskWithRedux>;
+type Story = StoryObj<typeof Button>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-
-export const TaskIsNotDone: Story = {
+export const Primary: Story = {
+  // More on args: https://storybook.js.org/docs/react/writing-stories/args
   args: {
-    tasks: {id: '12313jfdf', title: 'Lost', isDone: true},
-    todolistID: 'a123dsad1'
-  }
-}
-export const TaskisDone: Story = {
-  // args: {
-  //   label: 'Button'
-  // }
-}
+    primary: true,
+    label: 'Button',
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    label: 'Button',
+  },
+};
+
+export const Large: Story = {
+  args: {
+    size: 'large',
+    label: 'Button',
+  },
+};
+
+export const Small: Story = {
+  args: {
+    size: 'small',
+    label: 'Button',
+  },
+};
