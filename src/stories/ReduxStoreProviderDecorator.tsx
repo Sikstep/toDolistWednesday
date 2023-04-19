84a9a6a7f88d8f8fce5a549c4515228ab377eb52
Sikstep <Sikstep1988@gmail.com>
2023-04-19 20:52:12 +0300

    Wednesday lesson #4 storyBook + addItemForm stories + Tasks stories + EditableSpan.stories.ts + AppWithRedux.stories.tsx + TaskStory

--- src/stories/ReduxStoreProviderDecorator.tsx
+++ src/stories/ReduxStoreProviderDecorator.tsx
@@ -1,7 +1,34 @@
 import React from 'react';
 import {Provider} from 'react-redux';
-import {store} from '../state/store';
+import {AppRootStateType, store} from '../state/store';
+import {combineReducers, createStore, legacy_createStore} from 'redux';
+import { tasksReducer } from '../state/tasks-reducer';
+import { todolistsReducer } from '../state/todolists-reducer';
+import {v1} from 'uuid';
 
+const rootReducer = combineReducers({
+    tasks: tasksReducer,
+    todolists: todolistsReducer
+})
+
+const initialGlobalState = {
+    todolists: [
+        {id: "todolistId1", title: "What to learn", filter: "all"},
+        {id: "todolistId2", title: "What to buy", filter: "all"}
+    ] ,
+    tasks: {
+        ["todolistId1"]: [
+            {id: v1(), title: "HTML&CSS", isDone: false},
+            {id: v1(), title: "JS", isDone: true}
+        ],
+        ["todolistId2"]: [
+            {id: v1(), title: "Milk", isDone: true},
+            {id: v1(), title: "React Book", isDone: false}
+        ]
+    }
+};
+
+export const storyBookStore = legacy_createStore(rootReducer, initialGlobalState as AppRootStateType);
 export const ReduxStoreProviderDecorator = (storyFn: () => React.ReactNode) => {
-    return <Provider store={store}>{storyFn()}</Provider>
+    return <Provider store={storyBookStore}>{storyFn()}</Provider>
 }
--- src/stories/Tasks.stories.tsx
+++ src/stories/Tasks.stories.tsx
@@ -1,9 +1,13 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import {action} from '@storybook/addon-actions';
+import React from 'react';
 
 import { Button } from './Button';
 import {TaskWithRedux} from '../TaskWithRedux';
 import {TaskType} from '../TodolistWithRedux';
+import {ReduxStoreProviderDecorator} from './ReduxStoreProviderDecorator';
+import {useSelector} from 'react-redux';
+import {AppRootStateType} from '../state/store';
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta: Meta<typeof TaskWithRedux> = {
@@ -12,24 +16,22 @@ const meta: Meta<typeof TaskWithRedux> = {
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
   tags: ['autodocs'],
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  argTypes: {
-    // backgroundColor: { control: 'color' },
+  args: {
+    tasks: {id: 'sasdad', title: 'JS', isDone: true},
+    todolistID: 'todolistId1'
   },
+  decorators: [ReduxStoreProviderDecorator]
 };
 
 export default meta;
 type Story = StoryObj<typeof TaskWithRedux>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-
-export const TaskIsNotDone: Story = {
-  args: {
-    tasks: {id: '12313jfdf', title: 'Lost', isDone: true},
-    todolistID: 'a123dsad1'
-  }
+const TaskCopy = () => {
+  const task = useSelector<AppRootStateType, TaskType>(state => state.tasks['todolistId1'][0])
+  return <TaskWithRedux tasks={task} todolistID={'todolistId1'}/>
 }
-export const TaskisDone: Story = {
-  // args: {
-  //   label: 'Button'
-  // }
+
+export const TaskWithReduxStory: Story = {
+ render: () => <TaskCopy/>
 }
